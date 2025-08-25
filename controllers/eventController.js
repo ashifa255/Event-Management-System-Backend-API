f82df4739a3aab1ba1088dd5b192da652c6eb4bc
ashifa255 <astammannahifa0330@gmail.com>
2025-08-25 18:44:21 +0600

    Create eventController.js

---
+++ controllers/eventController.js
@@ -0,0 +1,144 @@
+const Event = require('../models/Event');
+
+// Create event
+exports.createEvent = async (req, res, next) => {
+  try {
+    // Add user to req.body
+    req.body.createdBy = req.user.id;
+
+    // If file is uploaded
+    if (req.file) {
+      req.body.eventBanner = req.file.path;
+    }
+
+    const event = await Event.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: event
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+// Get all events
+exports.getEvents = async (req, res, next) => {
+  try {
+    const events = await Event.find().populate('createdBy', 'name email');
+
+    res.status(200).json({
+      success: true,
+      count: events.length,
+      data: events
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+// Get single event
+exports.getEvent = async (req, res, next) => {
+  try {
+    const event = await Event.findById(req.params.id).populate('createdBy', 'name email');
+
+    if (!event) {
+      return res.status(404).json({
+        success: false,
+        message: 'Event not found'
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: event
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+// Update event
+exports.updateEvent = async (req, res, next) => {
+  try {
+    let event = await Event.findById(req.params.id);
+
+    if (!event) {
+      return res.status(404).json({
+        success: false,
+        message: 'Event not found'
+      });
+    }
+
+    // Make sure user is event owner
+    if (event.createdBy.toString() !== req.user.id) {
+      return res.status(401).json({
+        success: false,
+        message: 'Not authorized to update this event'
+      });
+    }
+
+    // If file is uploaded
+    if (req.file) {
+      req.body.eventBanner = req.file.path;
+    }
+
+    event = await Event.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      success: true,
+      data: event
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+// Delete event
+exports.deleteEvent = async (req, res, next) => {
+  try {
+    const event = await Event.findById(req.params.id);
+
+    if (!event) {
+      return res.status(404).json({
+        success: false,
+        message: 'Event not found'
+      });
+    }
+
+    // Make sure user is event owner
+    if (event.createdBy.toString() !== req.user.id) {
+      return res.status(401).json({
+        success: false,
+        message: 'Not authorized to delete this event'
+      });
+    }
+
+    await Event.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      data: {}
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
